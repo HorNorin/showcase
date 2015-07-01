66589d74b853c361d399a0b962b79de4a4a097d5
Hor Norin <hor.norin@framgia.com>
2015-07-01 14:03:00 +0700

    fix admin and user signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
   root "statics#index"
   
-  devise_for :user, controllers: { registrations: "registrations" }
+  devise_for :admin, class_name: "User", controllers: { sessions: "admin/sessions" }, only: :sessions
+  namespace :admin do
+    root "shows#index"
+    resources :shows
+  end
   
-  devise_for :admin, class_name: "User", controllers: { sessions: "admin/sessions" }
+  devise_for :user, controllers: { registrations: "registrations" }
+  devise_scope :user do
+    get "/sign_in" => "devise/sessions#new", as: :sign_in
+    get "/sign_out" => "devise/sessions#destroy", as: :sign_out
+    get "/sign_up" => "registrations#new", as: :sign_up
+  end
   
   get "/auth/twitter/callback" => "omniauth_callbacks#twitter"
   get "/auth/facebook/callback" => "omniauth_callbacks#facebook"
-  
-  namespace :admin do
-    resources :shows
-  end
 end
